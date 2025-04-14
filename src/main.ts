1f4b2b841445ba966df7b99510458ad9e1342c46
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-04-14 21:27:52 +0200

    reset cli after done

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,37 @@
 
 import { program } from "commander";
 import { addCommands } from "./commands.js";
-import logger, { loadingSpinner } from "./logger.js";;
+import logger, { loadingSpinner } from "./logger.js";
 import { maybeCreateKiDirAtHomeDir } from "./storage/index.js";
+import { runCommand } from "./exec.js";
 
-process.on("uncaughtException", error=>{
-    logger.fatal(error)
-})
+process.on("uncaughtException", error => {
+    logger.fatal(error);
+});
 
-process.on("uncaughtExceptionMonitor", error=>{
-    logger.fatal(error)
-})
+process.on("uncaughtExceptionMonitor", error => {
+    logger.fatal(error);
+});
 
-
-process.on("unhandledRejection", error=>{
-    logger.fatal(error)
-})
+process.on("unhandledRejection", error => {
+    logger.fatal(error);
+});
 const parseAndRun = async () => {
-    maybeCreateKiDirAtHomeDir();
+    try {
+        maybeCreateKiDirAtHomeDir();
 
-    addCommands(program);
-    await program.parseAsync();
-    loadingSpinner.stop();
-    console.log("\n\n");
+        addCommands(program);
+        await program.parseAsync();
+        loadingSpinner.stop();
+        console.log("\n\n");
+    } catch (error) {
+        throw error;
+    } finally {
+        runCommand("reset", {
+            stdio: "inherit",
+            encoding: "utf-8",
+        });
+    }
 };
 
 await parseAndRun();
